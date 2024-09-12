678a5137c8b2b1cd073a44a3f7e209eeef9abe2d
Shahin Mulani <102217599+Shahinmulani@users.noreply.github.com>
2024-09-12 12:11:08 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,81 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+/*  cout << "namaste dunia" <<endl;
+
+   int a = 123;
+   cout << a <<endl;
+*/ 
+
+/* Datatypes and variables
+ 
+   int a = 123;
+   cout<< a <<endl;
+  
+   char b = 'v';
+   cout << b <<endl;
+
+   bool bl = true;
+   cout << bl <<endl;
+
+   float f = 1.2;
+   cout << f <<endl;
+
+   double d =1.23;
+   cout<< d <<endl;
+
+   int size= sizeof(d);
+   cout << size <<endl;
+*/
+
+/* Typecasting
+
+    int a = 'a';
+    cout << a <<endl;
+
+    char ch = 98;
+    cout << ch <<endl;
+*/
+
+/*  unsigned int a = 123;    
+    cout << a << endl;     //unsigned is used to stored only positive values
+*/ 
+   
+//operators
+/*  int a = 2/5;
+   cout << a <<endl;
+
+   float b = 0.2/5;
+   cout << b <<endl;
+*/
+
+//Relational opertors
+
+/*int a = 2;
+int b = 5;
+
+bool first = (a==b);
+cout << first <<endl;
+
+bool second = (a<b);
+cout << second <<endl;
+
+bool third = (a>b);
+cout << third <<endl;
+
+bool fourth = (a<=b);
+cout << fourth <<endl;
+
+bool fifth = (a>=b);
+cout << fifth <<endl;       //0= false 1= true
+*/
+
+//Logical operator
+// int a = 21;
+// cout << !a <<endl;
+
+
+}
